9483ed79faf8e20aa3432020fea38deb5f489d8e
Jason Katsaros <katsarjp@tutanota.com>
2023-04-13 15:40:26 -0400

    refactor: create and attach security groups to ec2 instance (terraform)

--- ec2/main.tf
+++ ec2/main.tf
@@ -13,7 +13,39 @@ provider "aws" {
   region = "us-east-2"
 }
 
+resource "aws_security_group" "ccp-ec2-ssh-sg-terraform" {
+  name        = "ccp-ec2-ssh-sg-terraform"
+  description = "Allow SSH to Certified Cloud Practitioner EC2 Instances (Terraform)"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "ccp-ec2-sg-terraform" {
+  name        = "ccp-ec2-sg-terraform"
+  description = "Allow HTTP to Certified Cloud Practitioner EC2 Instances (Terraform)"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "ccp-ec2-terraform" {
   ami           = "ami-0103f211a154d64a6"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.ccp-ec2-ssh-sg-terraform.id, aws_security_group.ccp-ec2-sg-terraform.id]
 }
